a422e8b22f98e9dcd3b835a3934eae2b94875aa1
Nate Woods <Nate.Woods@workiva.com>
2014-10-23 04:36:30 -0600

    Adding forms support (thanks forms.brace.io)

--- js/bootswatch.js
+++ js/bootswatch.js
@@ -4,4 +4,28 @@
    */
   $('[data-toggle="popover"]').popover();
   $('[data-toggle="tooltip"]').tooltip();
+
+  $('#contact-form').submit(function (event) {
+    event.preventDefault();
+
+    var form = $(this);
+    var btn = $('[type=submit]', this).button('loading');
+
+    var data = form.serializeArray().reduce(function (hash, ele) {
+      hash[ele.name] = ele.value;
+      return hash;
+    }, {});
+
+    $.ajax({
+      url: form.attr('action'),
+      method: "POST",
+      data: data,
+      dataType: "json"
+    }).done(function(data) {
+      // TODO: verify response
+      console.log(data);
+    }).always(function () {
+      btn.button('reset')
+    });
+  });
 })(jQuery);
